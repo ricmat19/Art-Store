13b0c1895f146390fa1f6bde7e852611ccff13cc
Ricky Del Cueto <ric19arthouse@gmail.com>
2021-06-24 19:23:44 -0400

    Style Updates

--- client/src/components/contact.jsx
+++ client/src/components/contact.jsx
@@ -5,28 +5,28 @@ const ContactC = () => {
         <div>
             <div className="main-body">
                 <div className="center">
-                    <h1 className="subtitle">CONTACT</h1>
+                    <h1 className="subtitle">contact</h1>
                 </div>
                 <div className="form-div">
                     <form className="contact-form">
                         <div className="subject-line">
-                            <label className="form-label">Name</label>
+                            <label className="form-label">name</label>
                             <input type="text" name="name" className="form-control"/>
                         </div>
                         <div className="subject-line">
-                            <label className="form-label">Email</label>
+                            <label className="form-label">email</label>
                             <input type="email" name="email" className="form-control" required/>
                         </div>
                         <div className="subject-line">
-                            <label className="form-label">Subject</label>
+                            <label className="form-label">subject</label>
                             <input type="text" name="subject" className="form-control" required/>
                         </div>
                         <div className="subject-line">
-                            <label className="form-label">Message</label>
+                            <label className="form-label">message</label>
                             <textarea name="message" rows="10" required></textarea>
                         </div>
                         <div className="form-button-div text-center">
-                            <button type="submit" className="btn form-button">Submit</button>
+                            <button type="submit" className="btn form-button">submit</button>
                         </div>
                     </form>
                 </div>
--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -6,14 +6,14 @@ const HeaderC = () => {
             <div className="center">
                 <input type="checkbox" id="nav-toggle" className="nav-toggle"/>
                 <label className="nav-toggle-label">
-                    <a className="menu-toggle"><h1>Menu</h1></a>
+                    <a className="menu-toggle"><h1>menu</h1></a>
                 </label>
                 <nav className="navbar">
-                    <a href="/"><h1>HOME</h1></a>
-                    <a href="/collection/comics"><h1>STORE</h1></a>
-                    <a href="/about"><h1>INFO</h1></a>
-                    <a href="/contact"><h1>CONTACT</h1></a>
-                    <a href=""><h1>SIGNIN</h1></a>
+                    <a href="/"><h1>home</h1></a>
+                    <a href="/collection/comics"><h1>store</h1></a>
+                    <a href="/about"><h1>info</h1></a>
+                    <a href="/contact"><h1>contact</h1></a>
+                    <a href=""><h1>signin</h1></a>
                 </nav>
             </div>
             <hr/>
