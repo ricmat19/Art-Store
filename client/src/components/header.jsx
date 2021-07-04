2167ca41421a08b00b4e7883a35874eef8a4d9a1
Ricky Del Cueto <ric19arthouse@gmail.com>
2021-07-04 18:40:04 -0400

    Signin Modal

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const HeaderC = () => {
+
+    const [active, setActive] = useState(false);
+
+    let signinClass = "";
+
+    if(active === false){
+        signinClass = "signin-bg";
+    }else{
+        signinClass = "signin-bg signin-active";
+    }
+
+    const signinModal = (e) => {
+        setActive(true);
+    }
+
     return(
         <div className="navbar-div">
+            <div className={signinClass}>
+                <div className="signin-content">
+                    <h1 className="signin-header">welcome</h1>
+                    <div className="signin-input">
+                        <div>
+                            <input type="text" placeholder="Email"></input>
+                        </div>
+                        <div>
+                            <input type="text" placeholder="Password"></input>
+                        </div>
+                    </div>
+                    <div>
+                        <button>sign in</button>
+                    </div>
+                    <div className="signin-footer">
+                        <a href=""><span>forgot password?</span></a>
+                        <a href=""><span>create account</span></a>
+                    </div>
+                </div>
+            </div>
             <div>
                 <input type="checkbox" id="nav-toggle" className="nav-toggle"/>
                 <label className="nav-toggle-label">
@@ -13,7 +48,7 @@ const HeaderC = () => {
                     <a href="/collection/comic"><p className="nav-title">store</p></a>
                     <a href="/about"><p className="nav-title">info</p></a>
                     <a href="/contact"><p className="nav-title">contact</p></a>
-                    <a href=""><p className="nav-title">signin</p></a>
+                    <a onClick={signinModal} href=""><p className="nav-title">signin</p></a>
                 </nav>
             </div>
             <hr/>
