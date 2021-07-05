3e2a2102d05680634fc7aea96e2383deea5c2997
Ricky Del Cueto <ric19arthouse@gmail.com>
2021-07-05 19:10:13 -0400

    Signup & Forgot Password Modal

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -4,44 +4,90 @@ const HeaderC = () => {
 
     const [active, setActive] = useState(false);
 
-    let signinClass = "";
+    let signClass = "";
 
     if(active === false){
-        signinClass = "signin-bg";
+        signClass = "sign-bg";
     }else{
-        signinClass = "signin-bg signin-active";
+        signClass = "sign-bg sign-active";
     }
 
-    const signinShow = (e) => {
+    const signShow = (e) => {
         setActive(true);
     }
 
-    const signinNoShow = (e) => {
+    const signNoShow = (e) => {
         setActive(false);
     }
 
     return(
         <div className="navbar-div">
-            <div className={signinClass} onClick={e => signinNoShow()}>
-                <div className="signin-content">
-                    <h1 className="signin-header">welcome</h1>
-                    <div className="signin-input">
-                        <div>
+
+            <div className={signClass} onClick={e => signNoShow()}>
+                <div className="sign-content">
+                    <h1 className="sign-header">Create Account</h1>
+                    <div className="sign-input">
+                        <div className="name-input-div">
+                            <input type="text" placeholder="First Name"/>
+                            <input type="text" placeholder="Last Name"/>
+                        </div>
+                        <div className="modal-input-div">
+                            <input type="email" placeholder="Email"/>
+                        </div>
+                        <div className="modal-input-div">
+                            <input type="password" placeholder="Create Password"/>
+                        </div>
+                        <div className="modal-input-div">
+                            <input type="password" placeholder="Re-type Password"/>
+                        </div>
+                    </div>
+                    <div>
+                        <button>Create Account</button>
+                    </div>
+                    <div className="sign-footer">
+                        <span>Already have an account?<a href="">Sign In</a></span>
+                    </div>
+                </div>
+            </div>
+
+            <div className={signClass} onClick={e => signNoShow()}>
+                <div className="sign-content">
+                    <h1 className="sign-header">welcome</h1>
+                    <div>
+                        <div className="modal-input-div">
                             <input type="text" placeholder="Email"></input>
                         </div>
-                        <div>
+                        <div className="modal-input-div">
                             <input type="text" placeholder="Password"></input>
                         </div>
                     </div>
                     <div>
                         <button>sign in</button>
                     </div>
-                    <div className="signin-footer">
+                    <div className="sign-footer">
                         <a href=""><span>forgot password?</span></a>
                         <a href=""><span>create account</span></a>
                     </div>
                 </div>
             </div>
+
+            <div className={signClass} onClick={e => signNoShow()}>
+                <div className="sign-content">
+                    <h1 className="sign-header">Reset Password</h1>
+                    <div className="sign-input">
+                        <div className="forgot-input-div">
+                            <input type="text" placeholder="Email"/>
+                        </div>
+                    </div>
+                    <div>
+                        <button>Send Reset Link</button>
+                    </div>
+                    <div className="sign-footer">
+                        <span><a href="">Back to signin in</a></span>
+                    </div>
+                </div>
+            </div>
+
             <div>
                 <input type="checkbox" id="nav-toggle" className="nav-toggle"/>
                 <label className="nav-toggle-label">
@@ -52,7 +98,7 @@ const HeaderC = () => {
                     <a href="/collection/comic"><p className="nav-title">store</p></a>
                     <a href="/about"><p className="nav-title">info</p></a>
                     <a href="/contact"><p className="nav-title">contact</p></a>
-                    <p className="nav-title pointer" onClick={signinShow}>signin</p>
+                    <p className="nav-title pointer" onClick={signShow}>sign in</p>
                 </nav>
             </div>
             <hr/>
