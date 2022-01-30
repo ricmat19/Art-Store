28a9ac59030a19bbe2f934bd47789490fd62361b
Ricardo Del Cueto <ric19mat@gmail.com>
2022-01-30 17:45:00 -0500

    Remove images from .gitignore

--- client/src/components/about.jsx
+++ client/src/components/about.jsx
@@ -51,7 +51,7 @@ const AboutC = () => {
               <div className="big-image-div">
                 <img
                   className="big-image"
-                  src="../images/profileImage.png"
+                  src="../images/profile-image.jpg"
                   alt="Profile"
                 />
               </div>
