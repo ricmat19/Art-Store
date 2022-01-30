d524918769a5f0116202bd07b545c3ecf18408b6
Ricardo Del Cueto <ric19mat@gmail.com>
2022-01-30 12:38:49 -0500

    Updating DB File

--- db/database.sql
+++ db/database.sql
@@ -1,30 +1,18 @@
 CREATE DATABASE store;
 
-CREATE TABLE collection(
+CREATE TABLE products(
     id BIGSERIAL PRIMARY KEY,
     title VARCHAR(50),
     product VARCHAR(50),
-    images BYTEA,
     price MONEY,
-    info VARCHAR(300)
+    info VARCHAR(300),
+    imagekey VARCHAR(300)
 );
 
-CREATE TABLE users(
-    email VARCHAR(50) PRIMARY KEY,
-    password VARCHAR(50),
-    firstName VARCHAR(100),
-    lastName VARCHAR(100),
-    iv VARCHAR(255),
-    cart VARCHAR[]
-);
-
-CREATE TABLE shipment(
+CREATE TABLE cart(
     id BIGSERIAL PRIMARY KEY,
-    shipment VARCHAR[],
-    address VARCHAR(255),
-    suite VARCHAR(10),
-    city VARCHAR(255),
-    state VARCHAR(255),
-    zipcode VARCHAR(50),
-    phone VARCHAR(50)
+    cart VARCHAR[],
+    email VARCHAR(255),
+    qty VARCHAR[]
 );
+
