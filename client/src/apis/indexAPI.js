a5f780ed91202cc3893d15cd20b1e27fca5f688d
Ricardo Del Cueto <ric19mat@gmail.com>
2022-01-31 00:12:30 -0500

    Fix Environment Variable

--- client/src/apis/indexAPI.js
+++ client/src/apis/indexAPI.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   withCredentials: true,
-  baseURL: process.env.REACT_APP_PLANNER_API,
+  baseURL: process.env.REACT_APP_ARTSTORE_API,
 });
