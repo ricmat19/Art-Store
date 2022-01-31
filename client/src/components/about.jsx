725ee0671492aa73d5ddb0cf745d4116d0e65288
Ricardo Del Cueto <ric19mat@gmail.com>
2022-01-30 22:40:52 -0500

    Fixing Profile Image

--- client/src/components/about.jsx
+++ client/src/components/about.jsx
@@ -51,7 +51,7 @@ const AboutC = () => {
               <div className="big-image-div">
                 <img
                   className="big-image"
-                  src="/images/profile-image.jpg"
+                  src="/img/profile-image.jpg"
                   alt="Profile"
                 />
               </div>
